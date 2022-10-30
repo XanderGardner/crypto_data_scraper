f9a6db652ed0d83115bf98d804f3bfe7feae97b7
Xander Gardner <xandergardner7@gmail.com>
2022-10-30 11:53:47 -0400

    discord executable capable

--- discord.py
+++ discord.py
@@ -1,4 +1,6 @@
 import os
+import pathlib
+import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -55,69 +57,89 @@ def run_threads(threads):
       threads[i].join()
     run_threads(threads[groups:])
 
-# input and output to threads
-crypto_codes = [
-  ("Ethereum", "CetY6Y4"),
-  ("Cardano", "TUr9RDU"),
-  ("Solana", "pquxPsq"),
-  ("Avalanche", "RwXY7P6"),
-  ("Binance Chain", "bnbchain"),
-  ("Tron", "tron"),
-  ("Algorand", "84AActu3at"),
-  ("Near", "UY9Xf2k"),
-  ("Flow", "flow"),
-  ("Polygon", "XvpHAxZ"),
-  ("Polkadot", "CarTrFyppf"),
-  ("Cosmos", "cosmosnetwork"),
-  ("Tezos", "yXaPy6s5Nr"),
-  ("Internet Computer", "rB96MMn"),
-  ("Elrond", "VsmrGNWjND"),
-  ("Thor Chain", "tW64BraTnX"),
-  ("EOS", "eos-network"),
-  ("Harmony", "rdCmBpe"),
-  ("Fantom", "zS4Ek4WByA"),
-  ("Bitcoin", "okcash")
-]
+# returns resource path for users environment given the relative path
+def resource_path(relative_path):
+  try:
+    base_path = sys._MEIPASS
+  except Exception:
+    base_path = os.path.dirname(__file__)
+  return os.path.join(base_path, relative_path)
 
-# create threads and run with helper function
-d = OrderedDict()
-threads = []
-for code in crypto_codes:
-  d[code[0]] = []
-  threads += [Thread(target=get_members, args=(code, d))]
-run_threads(threads)
+def main():
+  # input and output to threads
+  crypto_codes = [
+    ("Ethereum", "CetY6Y4"),
+    ("Cardano", "TUr9RDU"),
+    ("Solana", "pquxPsq"),
+    ("Avalanche", "RwXY7P6"),
+    ("Binance Chain", "bnbchain"),
+    ("Tron", "tron"),
+    ("Algorand", "84AActu3at"),
+    ("Near", "UY9Xf2k"),
+    ("Flow", "flow"),
+    ("Polygon", "XvpHAxZ"),
+    ("Polkadot", "CarTrFyppf"),
+    ("Cosmos", "cosmosnetwork"),
+    ("Tezos", "yXaPy6s5Nr"),
+    ("Internet Computer", "rB96MMn"),
+    ("Elrond", "VsmrGNWjND"),
+    ("Thor Chain", "tW64BraTnX"),
+    ("EOS", "eos-network"),
+    ("Harmony", "rdCmBpe"),
+    ("Fantom", "zS4Ek4WByA"),
+    ("Bitcoin", "okcash")
+  ]
 
-# get current time
-dt = datetime.now()
-time_string = dt.strftime("%m-%d-%y %I%p %z")[:-1]
-
-# check output csv file
-path = 'discord_data.csv'
-exists = os.path.exists(path)
-file = open(path, 'a')
-if not exists:
-  # create top header row
-  file.write("Type,Time,")
+  # create threads and run with helper function
+  d = OrderedDict()
+  threads = []
   for code in crypto_codes:
-    file.write(f"{code[0]},")
+    d[code[0]] = []
+    threads += [Thread(target=get_members, args=(code, d))]
+  run_threads(threads)
+  print(f"found all values: {d}")
+
+  # get current time
+  dt = datetime.now()
+  time_string = dt.strftime("%m-%d-%y %I%p %z")[:-1]
+
+  # check output csv file
+  if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+    print('running in a PyInstaller bundle')
+    rel_path = os.path.dirname(sys.executable)
+    path = os.path.join(rel_path, 'discord_data.csv')
+  else:
+    print('running in a normal Python process')
+    path = pathlib.Path(__file__).parent / 'discord_data.csv'
+  
+  exists = os.path.isfile(path)
+  file = open(path, 'a')
+  if not exists:
+    # create top header row
+    file.write("Type,Time,")
+    for code in crypto_codes:
+      file.write(f"{code[0]},")
+    file.write("\n")
+
+  # print to output csv
+  file.write(f"Online,{time_string},")
+  for key in d.keys():
+    # format active number
+    active = d[key][0][:-7]
+    active =  re.sub(",","",active)
+    file.write(f"{active},")
   file.write("\n")
 
-# print to output csv
-file.write(f"Online,{time_string},")
-for key in d.keys():
-  # format active number
-  active = d[key][0][:-7]
-  active =  re.sub(",","",active)
-  file.write(f"{active},")
-file.write("\n")
+  file.write(f"Members,{time_string},")
+  for key in d.keys():
+    # format members number
+    members = d[key][1][:-8]
+    members =  re.sub(",","",members)
+    file.write(f"{members},")
+  file.write("\n")
 
-file.write(f"Members,{time_string},")
-for key in d.keys():
-  # format members number
-  members = d[key][1][:-8]
-  members =  re.sub(",","",members)
-  file.write(f"{members},")
-file.write("\n")
+  #file.write(time_string + "\n")
+  file.close()
 
-#file.write(time_string + "\n")
-file.close()
+if __name__ == "__main__":
+  main()
